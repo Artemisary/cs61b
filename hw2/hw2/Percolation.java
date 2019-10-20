2017fa89b42fb6ef5429e07c3c15c2a817b37c22
Artemisary <779288319@qq.com>
2019-10-21 00:40:40 +0800

    finish HW2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -16,62 +16,65 @@ public class Percolation {
 
     public void open(int row, int col) {
         C = C + 1;
-        if (row == 1) {
-            if (OpenSet[N + col - 1] == 1)
-                a.union(N + col - 1, col - 1);
-            if (col == 1)
+        if (row == 0) {
+            if (OpenSet[N + col] == 1)
+                a.union(N + col, col);
+            if (col == 0)
                 if (OpenSet[1] == 1)
                     a.union(1, 0);
-            if (col == N)
+            if (col == N - 1)
                 if (OpenSet[N - 2] == 1)
                     a.union(N - 2, N - 1);
             for (int i = 0; i < 3; i++)
-                if (OpenSet[col - 2 + i] == 1)
-                    a.union(+col - 2 + i, col - 1);
-        } else if (row == N) {
-            if (OpenSet[(N - 2) * N + col - 1] == 1)
-                a.union((N - 2) * N + col - 1, (N - 1) * N + col - 1);
-            if (col == 1)
+                if (OpenSet[col - 1 + i] == 1)
+                    a.union(+col - 1 + i, col);
+        } else if (row == N - 1) {
+            if (OpenSet[(N - 2) * N + col] == 1)
+                a.union((N - 2) * N + col, (N - 1) * N + col);
+            if (col == 0)
                 if (OpenSet[(N - 1) * N + 1] == 1)
                     a.union((N - 1) * N + 1, (N - 1) * N);
-            if (col == N)
+            if (col == N - 1)
                 if (OpenSet[N * N - 1] == 1)
                     a.union(N * N - 1, N * N);
             for (int i = 0; i < 3; i++)
-                if (OpenSet[(N - 1) * N + col - 2 + i] == 1)
-                    a.union((N - 1) * N + col - 2 + i, (N - 1) * N + col - 1);
-        } else if (col == 1) {
+                if (OpenSet[(N - 1) * N + col - 1 + i] == 1)
+                    a.union((N - 1) * N + col - 1 + i, (N - 1) * N + col);
+        } else if (col == 0) {
             for (int i = 0; i < 3; i++)
-                if (OpenSet[(row - 2 + i) * N] == 1)
-                    a.union((row - 2 + i) * N, (row - 1) * N);
-            if (OpenSet[(row - 1) * N + 1] == 1)
-                a.union((row - 1) * N + 1, (row - 1) * N);
+                if (OpenSet[(row - 1 + i) * N] == 1)
+                    a.union((row - 1 + i) * N, row * N);
+            if (OpenSet[row * N + 1] == 1)
+                a.union(row * N + 1, row * N);
         } else if (col == N) {
             for (int i = 0; i < 3; i++)
-                if (OpenSet[(row - 1 + i) * N - 1] == 1)
-                    a.union((row - 1 + i) * N - 1, row * N - 1);
-            if (OpenSet[row * N - 2] == 1)
-                a.union(row * N - 2, row * N - 1);
+                if (OpenSet[(row + i) * N - 1] == 1)
+                    a.union((row + i) * N - 1, (row + 1) * N - 1);
+            if (OpenSet[(row + 1) * N - 2] == 1)
+                a.union((row + 1) * N - 2, (row + 1) * N - 1);
         } else {
             for (int i = 0; i < 3; i++) {
-                if (OpenSet[(row - 1) * N + col - 2 + i] == 1)
-                    a.union((row - 1) * N + col - 2 + i, (row - 1) * N + col - 1);
-                if (OpenSet[(row - 2 + i) * N + col - 1] == 1)
-                    a.union((row - 2 + i) * N + col - 1, (row - 1) * N + col - 1);
+                if (OpenSet[row * N + col - 1 + i] == 1)
+                    a.union(row * N + col - 1 + i, row * N + col);
+                if (OpenSet[(row - 1 + i) * N + col] == 1)
+                    a.union((row - 1 + i) * N + col, row * N + col);
             }
         }
-        OpenSet[(row - 1) * N + col - 1] = 1;
+        OpenSet[row * N + col] = 1;
     }// open the site (row, col) if it is not open already
 
     public boolean isOpen(int row, int col) {
-        if (OpenSet[(row - 1) * N + col - 1] == 1)
+        if (OpenSet[row * N + col] == 1)
             return true;
         return false;
     }  // is the site (row, col) open?
 
     public boolean isFull(int row, int col) {
+        if (row == 0)
+            if (!this.isOpen(row, col))
+                return false;
         for (int i = 0; i < N; i++) {
-            if (a.connected(i, (row - 1) * N + col - 1))
+            if (a.connected(i, row * N + col))
                 return true;
         }
         return false;
@@ -93,19 +96,19 @@ public class Percolation {
     public static void main(String[] args) {
         Percolation test = new Percolation(4);
         System.out.println(test.isOpen(3, 4));
-        test.open(3, 4);
-        System.out.println(test.isOpen(3, 4));
-        test.open(2, 4);
-        test.open(2, 2);
-        test.open(2, 1);
         test.open(2, 3);
-        test.open(4, 3);
-        System.out.println(test.isFull(3, 4));
+        System.out.println(test.isOpen(3, 4));
+        test.open(1, 3);
+        test.open(1, 1);
+        test.open(1, 0);
         test.open(1, 2);
-        System.out.println(test.isFull(3, 4));
-        System.out.println(test.isFull(2, 1));
+        test.open(3, 2);
+        System.out.println(test.isFull(2, 3));
+        test.open(0, 1);
+        System.out.println(test.isFull(2, 3));
+        System.out.println(test.isFull(1, 0));
         System.out.println(test.percolates());
-        test.open(3, 3);
+        test.open(2, 2);
         System.out.println(test.percolates());
     }   // use for unit testing (not required, but keep this here for the autograder)
 
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,51 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+
 public class PercolationStats {
+    PercolationFactory a = new PercolationFactory();
+    Percolation b;
+    int[] x;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N < 1 || T < 1) {
+            throw new java.lang.IllegalArgumentException("We need a positive number!");
+        }
+        b = a.make(N);
+        x = new int[T];
+        for (int i = 0; i < T; i++) {
+            int c = StdRandom.uniform(N * N);
+            while (!b.percolates()) {
+                while (b.isOpen(c / 4, c - c / 4 + 1))
+                    c = StdRandom.uniform(N * N);
+                b.open(c / 4, c - c / 4 + 1);
+            }
+            x[i] = b.numberOfOpenSites();
+        }
+    }   // perform T independent experiments on an N-by-N grid
+
+    public double mean() {
+        double m = 0;
+        for (int i = 0; i < x.length; i++) {
+            m = (double) x[i] + m;
+        }
+        return m / (double) x.length;
+    }                                           // sample mean of percolation threshold
+
+    public double stddev() {
+        double s = 0;
+        for (int i = 0; i < x.length; i++) {
+            s = ((double) x[i] - this.mean()) * ((double) x[i] - this.mean()) + s;
+        }
+        return Math.sqrt(s / (double) (x.length - 1));
+    }                                         // sample standard deviation of percolation threshold
+
+    public double confidenceLow() {
+        return this.mean() - 1.96 * this.stddev() / Math.sqrt((double) x.length - 1);
+    }                                  // low endpoint of 95% confidence interval
+
+    public double confidenceHigh() {
+        return this.mean() + 1.96 * this.stddev() / Math.sqrt((double) x.length - 1);
+    }                                 // high endpoint of 95% confidence interval
 
 }
